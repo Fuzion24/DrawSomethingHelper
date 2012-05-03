a40d3a01dbc163cf354b1d56f3c0df5c8ab98256
Ryan Welton <rwelton24@gmail.com>
2012-05-03 17:23:21 -0400

    Results from google imgae search

--- src/com/fuzionsoftware/alert/system/OverlayService.java
+++ src/com/fuzionsoftware/alert/system/OverlayService.java
@@ -2,12 +2,14 @@ package com.fuzionsoftware.alert.system;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Binder;
 import android.os.IBinder;
 import android.view.View;
 import android.view.WindowManager;
@@ -16,19 +18,23 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class OverlayService extends Service implements SensorEventListener{
-  	private SensorManager sensorMgr;
-    private long lastUpdate = -1;
-    private float x, y, z;
+  	private SensorManager mSensorMgr;
+    private long mLastUpdate = -1;
+    private float mX, mY, mZ;
     private float last_x, last_y, last_z;
     private static final int SHAKE_THRESHOLD = 800;
 	
+	private final IBinder mBinder = new LocalBinder();
+	
+    private Bitmap mOverlayImage;
+    private int mOpacity = 70;
     private View mView;
     private boolean mOverlayEnabled = false;
     
 	@Override
 	public IBinder onBind(Intent intent) {
 		Toast.makeText(this, "Service binded...", Toast.LENGTH_LONG).show();
-		return null;
+		return mBinder;
 	}
 	
 	@Override
@@ -42,8 +48,8 @@ public class OverlayService extends Service implements SensorEventListener{
 	private void enableMotion()
 	{
 		 // start motion detection 
-		 sensorMgr=(SensorManager) getSystemService(SENSOR_SERVICE); 
-		 sensorMgr.registerListener(this, sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+		 mSensorMgr=(SensorManager) getSystemService(SENSOR_SERVICE); 
+		 mSensorMgr.registerListener(this, mSensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
 	}
 	
 	private void toggleOverlay()
@@ -69,7 +75,7 @@ public class OverlayService extends Service implements SensorEventListener{
 	{
 		ImageView iv = new ImageView(this);
 		Drawable img = this.getApplicationInfo().loadIcon(this.getPackageManager());
-		img.setAlpha(70);
+		img.setAlpha(mOpacity);
 		iv.setImageDrawable(img);
 		return iv;
 	}
@@ -78,24 +84,35 @@ public class OverlayService extends Service implements SensorEventListener{
 	public void onSensorChanged(SensorEvent event) {
 		 if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){ 
 			 	long curTime = System.currentTimeMillis(); // only allow one update every 500ms. 
-			 	if ((curTime - lastUpdate) > 100) { 
-			 		long diffTime = (curTime - lastUpdate); 
-			 		lastUpdate = curTime; x = event.values[0]; 
-			 		y = event.values[1]; z = event.values[2]; 
-			 		float speed = Math.abs(x+y+z - last_x - last_y - last_z) / diffTime * 10000; 
+			 	if ((curTime - mLastUpdate) > 100) { 
+			 		long diffTime = (curTime - mLastUpdate); 
+			 		mLastUpdate = curTime; mX = event.values[0]; 
+			 		mY = event.values[1]; mZ = event.values[2]; 
+			 		float speed = Math.abs(mX+mY+mZ - last_x - last_y - last_z) / diffTime * 10000; 
 			 		if (speed > SHAKE_THRESHOLD) { 
 			 			// yes, this is a shake action! Do something about it! 
 			 			//Toast.makeText(this, "Shake it like a polaroid picture...", Toast.LENGTH_LONG).show();
 			 			toggleOverlay();
 			 		} 
-			 		last_x = x; 
-			 		last_y = y; 
-			 		last_z = z; 
+			 		last_x = mX; 
+			 		last_y = mY; 
+			 		last_z = mZ; 
 			 	} 
 			 } 		
 	}
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+	
+	
+	class LocalBinder extends Binder {
+		
+		public void setOverlayImage()
+		{
+			
+		}
+			
+	}
+	
 	
 }
--- src/com/fuzionsoftware/alert/system/SystemAlertTestActivity.java
+++ src/com/fuzionsoftware/alert/system/SystemAlertTestActivity.java
@@ -1,30 +1,81 @@
 package com.fuzionsoftware.alert.system;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
+import android.net.UrlQuerySanitizer;
+import android.net.UrlQuerySanitizer.ValueSanitizer;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.IBinder;
 
 public class SystemAlertTestActivity extends Activity {
-    /** Called when the activity is first created. */
-	private Handler mHandler = new Handler();
+
+	private OverlayService.LocalBinder mService;
+	private ServiceConnection mServiceConnection = new ServiceConnection() {	
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder srv) {
+			mService = (OverlayService.LocalBinder) srv;
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			mService = null;
+		}
+    };
+    
+    public void searchGoogleImages(String searchParameters)
+    {
+		try {
+			
+			Uri.Builder uri = new Uri.Builder()
+				.scheme("https").authority("ajax.googleapis.com")
+				.appendEncodedPath("ajax/services/search/images")
+				.appendQueryParameter("v", "1.0")
+				.appendQueryParameter("q", searchParameters);
+
+			URLConnection connection = new URL(uri.toString()).openConnection();
+			String line = new java.util.Scanner(connection.getInputStream()).useDelimiter("\\A").next();
+			JSONObject json = new JSONObject(line);
+			//now have some fun with the results...
+			
+			json.get("test");
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mHandler.postDelayed(mUpdateTimeTask, 1000);
-    	Context ctx = getApplicationContext();
-        ctx.startService(new Intent(ctx, OverlayService.class));
+        setContentView(R.layout.main);
     }
     
-    private Runnable mUpdateTimeTask = new Runnable() {
-        public void run() {
-            //AlertDialog d = new AlertDialog.Builder(SystemAlertTestActivity.this).setTitle("tanchulai").setMessage("bucuo de tanchulai").create();
-            //d.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-            //d.show();     
-
-        }
-     };
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	unbindService(mServiceConnection);
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onPause();
+    	startService(new Intent(this, OverlayService.class));
+    	bindService(new Intent(this, OverlayService.class), mServiceConnection, Context.BIND_AUTO_CREATE);
+    	//mService.setOverlayImage();
+    	searchGoogleImages("fuzzy monkey");
+    }
+    
     }
 
 
