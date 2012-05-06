e785aa1397161a33d60685b22487d0f5fc80f9e5
Ryan Welton <rwelton24@gmail.com>
2012-05-05 21:36:32 -0400

    Refactored some codez

--- src/com/fuzionsoftware/alert/system/SystemAlertTestActivity.java
+++ src/com/fuzionsoftware/alert/system/SystemAlertTestActivity.java
@@ -1,7 +1,6 @@
 package com.fuzionsoftware.alert.system;
 
-import java.net.URL;
-import java.net.URLConnection;
+
 import java.util.ArrayList;
 
 import android.app.Activity;
@@ -9,7 +8,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -27,10 +25,8 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 
-import com.fuzionsoftware.googleimages.GoogleImageResponse;
-import com.fuzionsoftware.googleimages.GoogleImageResponse.GoogleImageResult;
+import com.fuzionsoftware.googleimages.GoogleImageSearch;
 import com.fuzionsoftware.imagecaching.UrlImageViewHelper;
-import com.google.gson.Gson;
 
 public class SystemAlertTestActivity extends Activity {
 
@@ -65,7 +61,7 @@ public class SystemAlertTestActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        setContentView(R.layout.main);
+        setContentView(R.layout.google_image_search);
 
         mGridView = (GridView) findViewById(R.id.gridview);
         mAdapter = new GoogleImageAdapter(this);
@@ -120,7 +116,7 @@ public class SystemAlertTestActivity extends Activity {
             @Override
             public void onClick(View v) {
             	hideKeyboard();
-            	new GoogleImageSearch().execute(mSearchText.getText().toString());
+            	new GoogleImageSearchTask().execute(mSearchText.getText().toString());
             }
         
     }
@@ -160,53 +156,15 @@ public class SystemAlertTestActivity extends Activity {
             return imageView;
         }
     }
-    private class GoogleImageSearch extends AsyncTask<String,Void,ArrayList<String>> {
+    private class GoogleImageSearchTask extends AsyncTask<String,Void,ArrayList<String>> {
 
     	protected void onPreExecute (){
     		mAdapter.clear();
     	}
     	@Override
     	protected ArrayList<String> doInBackground(String... searchParameters) {
-    		ArrayList<String> urls = new ArrayList<String>();
-    		int start = 0;
     		int count = 50;
-    		try {
-    			while(start < count)
-    			{
-    				//https://developers.google.com/image-search/v1/jsondevguide
-    				Uri.Builder uri = new Uri.Builder()
-    					.scheme("https").authority("ajax.googleapis.com")
-    					.appendEncodedPath("ajax/services/search/images")
-    					.appendQueryParameter("v", "1.0")
-    					.appendQueryParameter("imgsz", "large")
-    					.appendQueryParameter("imgtype", "clipart")
-    					/*
-    					Specifies the search safety level, which may be one of the following:
-
-    					safe=active enables the highest level of safe search filtering.
-    					safe=moderate enables moderate safe search filtering (default).			
-    					safe=off disables safe search filtering.
-    					*/
-    					.appendQueryParameter("safe", "off")
-    					.appendQueryParameter("start", Integer.toString(start))
-    					.appendQueryParameter("q", searchParameters[0]);
-    	
-    				URLConnection connection = new URL(uri.toString()).openConnection();
-    				
-    				//http://stackoverflow.com/questions/309424/in-java-how-do-i-read-convert-an-inputstream-to-a-string
-    				String json_results = new java.util.Scanner(connection.getInputStream()).useDelimiter("\\A").next();
-    				
-    		        GoogleImageResponse gImages = new Gson().fromJson(json_results, GoogleImageResponse.class);
-    		        
-    		        for(GoogleImageResult result : gImages.responseData.results)
-    		        	urls.add(result.url);
-    		        
-    		        start += gImages.responseData.results.length;
-    			}
-    		} catch (Exception e) {
-    			e.printStackTrace();
-    		}
-    		return urls;
+    		return GoogleImageSearch.searchGoogleImages(searchParameters[0], count);
     	}
     	
     	protected void onPostExecute (ArrayList<String> results){
